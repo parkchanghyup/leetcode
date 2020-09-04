607fa31637016325850b8ccd888af19a14ee8be6
park <ariz1623@naver.com>
2020-09-04 23:58:21 +0900

    리트코드

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        return strs == strs[::-1]
